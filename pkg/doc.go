6858cf2330d9828d86f9302466cef8a9024a2b06
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2022-11-23 11:14:49 +0200

    added go.mod to pkg

---
+++ pkg/doc.go
@@ -0,0 +1,11 @@
+/*
+ * Borsch Runner Service
+ *
+ * Copyright (C) 2022 Yuriy Lisovskiy - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the MIT license.
+ */
+
+// Package pkg exposes runner service functionality that may be required
+// by other projects.
+package pkg
